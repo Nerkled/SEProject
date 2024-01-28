e74484a456307e604c86d4d8a350efae4a98dc60
justinfei9 <jpf99@yahoo.com>
2024-01-28 15:47:30 -0500

    Update testingSetup.java

--- testingSetup.java
+++ testingSetup.java
@@ -4,6 +4,6 @@ public class testingSetup{
         int[] array = new int[2];
         array[-1]=0;  //This line should generate an error because -1 is out of bounds for the array.
         System.out.println("The value at index -1 in the array is: " + array[-1]);
-
+        //adding this so i can comit again
     }
 }
