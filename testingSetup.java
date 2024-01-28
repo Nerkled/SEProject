c2fe3176691da240be86517a18056a77a131713d
justinfei9 <jpf99@yahoo.com>
2024-01-28 15:06:58 -0500

    part 3 step 1
    I created a test java file that breaks because I call the -1 index of an array which is invalid. this should fail, then we will fix it later

---
+++ testingSetup.java
@@ -0,0 +1,9 @@
+public class testingSetup{
+    public static void main(String[] argas)
+    {
+        int[] array = new int[2];
+        array[-1]=0;  //This line should generate an error because -1 is out of bounds for the array.
+        System.out.println("The value at index -1 in the array is: " + array[-1]);
+
+    }
+}
