8f9c283939729c553f7483e16459d3053c577f19
Henry <henrybecker1014@gmail.com>
2024-01-28 16:13:24 -0500

    Update testingSetup.java
    Updated name of class and file name, updated indentation, deleted two lines

--- testingSetup.java
+++ testingSetup.java
@@ -4,14 +4,13 @@
 // Last modification by $Author: Justin Feinman $ 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-
-public class testingSetup{
-    public static void main(String[] argas)
-    {
+public class TestingSetup {
+    public static void main(String[] args) {
         int[] array = new int[2];
-        array[0]=9;
-        array[1]=10;
+
+        array[0] = 9;
+        array[1] = 10;
         System.out.println("The Value at 0 index is " + array[0] + "  value at index 1 is " + array[1]);
-        //working test class
+        // Working test class
     }
 }
