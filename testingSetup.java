36c2153c290b5f64cdd3d96d01e09c1b7d457853
justinfei9 <jpf99@yahoo.com>
2024-01-28 15:52:50 -0500

    step 3 part 4
    tester class should now be working. This shouldn't fail any  status checks and should work!!

--- testingSetup.java
+++ testingSetup.java
@@ -2,8 +2,9 @@ public class testingSetup{
     public static void main(String[] argas)
     {
         int[] array = new int[2];
-        array[-1]=0;  //This line should generate an error because -1 is out of bounds for the array.
-        System.out.println("The value at index -1 in the array is: " + array[-1]);
-        //adding this so i can comit again
+        array[0]=9;
+        array[1]=10;
+        System.out.println("The Value at 0 index is " + array[0] + "  value at index 1 is " + array[1]);
+        //working test class
     }
 }
