1198c727c03bc4d168d68132685019f6eb1e1da7
justinfei9 <jpf99@yahoo.com>
2024-01-28 19:11:36 -0500

    Update TestingSetup.java
    updated TestingSetup (feature branch) trying to get this to main

--- TestingSetup.java
+++ TestingSetup.java
@@ -1,8 +1,17 @@
+<<<<<<< Updated upstream
 // checkstyle:
 // Checks Java source code for adherence to a set of rules.
 // Copyright (C) 2024  Henry Becker
 // Last modification by $Author: Justin Feinman $ 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+=======
+public class TestingSetup{
+    public static void main(String[] argas)
+    {
+        int[] array = new int[2];
+        array[-1]=0;  //This line should generate an error because -1 is out of bounds for the array.
+        System.out.println("The value at index -1 in the array is: " + array[-1]);
+>>>>>>> Stashed changes
 
 public class TestingSetup {
     public static void main(String[] args) {
