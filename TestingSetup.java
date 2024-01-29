76d0d579f1d8c04b16dd2272b600997cc687e7d5
justinfei9 <147112015+justinfei9@users.noreply.github.com>
2024-01-28 20:02:42 -0500

    Update testingSetup.java (#3)
    * Update testingSetup.java
    
    * step 3 part 4
    
    tester class should now be working. This shouldn't fail any  status checks and should work!!
    
    * Update testingSetup.java
    
    added copyright checkstyle feature
    
    * Update testingSetup.java
    
    Updated name of class and file name, updated indentation, deleted two lines
    
    * Update testingSetup.java
    
    Changed code on two lines hoping it works
    
    * Rename testingSetup.class to TestingSetup.class
    
    Name change
    
    * Rename testingSetup.java to TestingSetup.java
    
    Changed Name
    
    * Update TestingSetup.java
    
    updated TestingSetup (feature branch) trying to get this to main
    
    * updated both javafile and setupfile
    
    updated TestingSetup (feature branch) trying to get this to main
    
    * Update TestingSetup.java
    
    Fixed Header
    
    * Update TestingSetup.java
    
    Removed REGEX Stuff now that they gave been dropped
    
    ---------
    
    Co-authored-by: Henry <henrybecker1014@gmail.com>
    Co-authored-by: Henry <125415992+Nerkled@users.noreply.github.com>

--- TestingSetup.java
+++ TestingSetup.java
@@ -1,9 +1,10 @@
-public class testingSetup{
-    public static void main(String[] argas)
-    {
-        int[] array = new int[2];
-        array[-1]=0;  //This line should generate an error because -1 is out of bounds for the array.
-        System.out.println("The value at index -1 in the array is: " + array[-1]);
 
+public class TestingSetup {
+    public static void main(String[] args) {
+        int[] array = new int[2];
+        array[0] = 9;
+        array[1] = 10;
+        System.out.println("The Value at 0 index is " + array[0] + "  value at index 1 is " + array[1]);
+        // Working test class
     }
 }
