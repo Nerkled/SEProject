60a6d5438beb2afd065144ebe001382386a26340
justinfei9 <147112015+justinfei9@users.noreply.github.com>
2024-02-11 16:21:10 -0500

    created implementation of UserToComputeEngineAPI (#21)
    * Update testingSetup.java
    
    * step 3 part 4
    
    tester class should now be working. This shouldn't fail any  status checks and should work!!
    
    * Update testingSetup.java
    
    added copyright checkstyle feature
    
    * Update testingSetup.java
    
    Updated name of class and file name, updated indentation, deleted two lines
    
    * Update testingSetup.java
    
    Changed code on two lines hoping it works
    
    * Rename testingSetup.class to TestingSetup.class
    
    Name change
    
    * Rename testingSetup.java to TestingSetup.java
    
    Changed Name
    
    * Update TestingSetup.java
    
    updated TestingSetup (feature branch) trying to get this to main
    
    * updated both javafile and setupfile
    
    updated TestingSetup (feature branch) trying to get this to main
    
    * Update TestingSetup.java
    
    Fixed Header
    
    * Update TestingSetup.java
    
    Removed REGEX Stuff now that they gave been dropped
    
    * Update README.md
    
    updated README to explain what we are going to be computing for our project.
    
    * Create userToComputeEngineAPI.java
    
    created the first API, this APIis An API between the user and the compute engine. The user will specify a source for the input, delimiter characters for the output, and a destination for the output.  This is currently not functional because source class and input class are not created yet
    
    * Update README.md
    
    Added image for Compute Engine.
    
    * added Input & source java classes
    
    added Input & source java classes with no implimentation. added a .gitignore to remove all .class files (was requested by the professor). also changed the api's enum to be less intensive
    
    * renamed UserToComputeEngineAPI to have camel case
    
    renamed userToComputeEngineAPI  to UserToComputeEngineAPI , hopefully this works
    
    * Delete TestingSetup.java
    
    * Delete TestingSetup.class
    
    * created second API reads in user specified data and writes it out to the computing engine
    
    * fixed whitespace error
    
    * Create LucasComputerAPI.java
    
    Commiting API for LucasNum
    
    * Create src
    
    trying to create src folder
    
    * Delete src
    
    * Create test
    
    * Create test
    
    * Rename Input.java to src/Input.java
    
    moved file into src folder
    
    * Rename LucasComputerAPI.java to src/LucasComputerAPI.java
    
    moved file into src folder
    
    * Rename Output.java to src/Output.java
    
    moved file into src folder
    
    * Rename Source.java to src/Source.java
    
    moved file into src folder
    
    * Rename StorageToComputeEngineAPI.java to src/StorageToComputeEngineAPI.java
    
    moved file into src folder
    
    * Rename UserToComputeEngineAPI.java to src/UserToComputeEngineAPI.java
    
    moved file into src folder
    
    * created implementation of UserToComputeEngineAPI
    
    created UserToComputeEngineImp  which is the implementation of UserToComputeEngineAPI
    
    * implemented storageToComputeEngineAPI
    
    created UserToComputeEngineImp to implement storageToComputeEngineAPI
    
    * Update UserToComputeEngineImp.java
    
    tried to fix some issues wwith checkstyle. hopefully this works
    
    ---------
    
    Co-authored-by: Henry <henrybecker1014@gmail.com>
    Co-authored-by: Henry <125415992+Nerkled@users.noreply.github.com>
    Co-authored-by: vincentbonet <141579416+vincentbonet@users.noreply.github.com>

---
+++ src/UserToComputeEngineImp.java
@@ -0,0 +1,37 @@
+public class UserToComputeEngineImp implements UserToComputeEngineAPI {
+
+    private Input input;
+    private Source source;
+    private char resultDelimiter;
+    private Source destination;
+    private DelimiterResponse option;
+    private StorageToComputeEngineAPI storageToComputeEngineAPI;
+
+    public UserToComputeEngineImp(StorageToComputeEngineAPI storageToComputeEngineAPI) {
+        this.storageToComputeEngineAPI = storageToComputeEngineAPI;
+    }
+
+    public void setInputSource(Source source) {
+        this.source = source;
+    }
+
+    public void setOutputDelimiter(char resultDelimiter) {
+        this.resultDelimiter = resultDelimiter;
+    }
+
+    public void setOutputDestination(Source destination) {
+        this.destination = destination;
+    }
+
+    public void defaultDelimiter(DelimiterResponse option) {
+        this.option = option;
+    }
+
+    public void setComputationInput(Input input) {
+        this.input = input;
+    }
+
+    public void executeLucasNumberComputation() {
+        // code to execute lucas number computation
+    }
+}
