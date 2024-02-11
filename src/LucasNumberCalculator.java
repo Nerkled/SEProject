63302a86f573f1ecf620005fcbb9c10b64034a44
Henry <henrybecker1014@gmail.com>
2024-02-11 16:26:51 -0500

    Had to redo it since it was pushed to main before. Woops

---
+++ src/LucasNumberCalculator.java
@@ -0,0 +1,42 @@
+public class LucasNumberCalculator implements LucasComputerAPI{
+
+    private StorageToComputeEngineAPI storageToComputeEngineAPI;
+
+    // TODO: Add any additional methods or functionality as needed
+
+    // 1: Initialize the API
+    public void initialize() {
+        this.initialize();
+    }
+
+    // 2: Get Write Data
+    public void read(Input input) {
+       this.read(input);
+    }
+
+    // 3: Write Data
+    public void write(Output output) {
+        this.write(output);
+    }
+
+    // 4: Pass Data through LucasComputingModule
+    public void computeLucas(Input input) {
+        this.computeLucas(input);
+    }
+
+    // 5: Get the result
+    public void getResult() {
+        // TODO: Implement the getResult() method
+        this.getResult();
+    }
+
+    // 6: Enumerate the result (Example result --> $result)
+    public void enumResult() {
+        this.enumResult();
+    }
+
+    // 7: Send back to StorageToComputeEngineAPI
+    public void sendResult() {
+        this.sendResult();
+    }
+}
