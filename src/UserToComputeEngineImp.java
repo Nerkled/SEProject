b83dccd41a77d66f898eadbc96f7a2717637158c
justinfei9 <jpf99@yahoo.com>
2024-02-11 16:09:54 -0500

    Update UserToComputeEngineImp.java
    tried to fix some issues wwith checkstyle. hopefully this works

--- src/UserToComputeEngineImp.java
+++ src/UserToComputeEngineImp.java
@@ -1,41 +1,37 @@
-public class UserToComputeEngineImp implements UserToComputeEngineAPI
-{
+public class UserToComputeEngineImp implements UserToComputeEngineAPI {
+
     private Input input;
     private Source source;
     private char resultDelimiter;
     private Source destination;
     private DelimiterResponse option;
-    private StorageToComputeEngineAPI storageToComputeEngineAPI; 
-    
+    private StorageToComputeEngineAPI storageToComputeEngineAPI;
 
-    public UserToComputeEngineImp(StorageToComputeEngineAPI storageToComputeEngineAPI)
-    {
+    public UserToComputeEngineImp(StorageToComputeEngineAPI storageToComputeEngineAPI) {
         this.storageToComputeEngineAPI = storageToComputeEngineAPI;
     }
-    
 
-    public void setInputSource(Source source)
-    {
+    public void setInputSource(Source source) {
         this.source = source;
     }
-    public void setOutputDelimiter(char resultDelimiter)
-    {
+
+    public void setOutputDelimiter(char resultDelimiter) {
         this.resultDelimiter = resultDelimiter;
     }
-    public void setOutputDestination(Source destination)
-    {
+
+    public void setOutputDestination(Source destination) {
         this.destination = destination;
     }
-    public void defaultDelimiter(DelimiterResponse option)
-    {
+
+    public void defaultDelimiter(DelimiterResponse option) {
         this.option = option;
     }
-    public void setComputationInput(Input input)
-    {
-        this.input=input;
+
+    public void setComputationInput(Input input) {
+        this.input = input;
     }
-    public void executeLucasNumberComputation()
-    {
-        //code to execute lucas number computation
+
+    public void executeLucasNumberComputation() {
+        // code to execute lucas number computation
     }
-} 
+}
