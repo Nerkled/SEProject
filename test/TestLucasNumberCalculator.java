0d246e1c435b411bb533f0e8081f539b20fe140c
Henry <henrybecker1014@gmail.com>
2024-02-12 23:47:33 -0500

    Not working but moving it to my other computer

---
+++ test/TestLucasNumberCalculator.java
@@ -0,0 +1,21 @@
+
+import org.junit.Test;
+import org.mockito.Mockito;
+import static org.mockito.Mockito.*;
+
+
+
+public class TestLucasNumberCalculator {
+
+    @Test
+    public void testCoordinatorSmoke() {
+        //Mocking the APIs
+        StorageToComputeEngineAPI computeEngine = Mockito.mock(StorageToComputeEngineAPI.class);
+        LucasComputerAPI lucasComputerAPI = Mockito.mock(LucasComputerAPI.class);
+        
+        
+        
+    }
+    
+    
+}
