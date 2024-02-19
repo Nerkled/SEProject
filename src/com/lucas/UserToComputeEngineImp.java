551fb711220adc78be1bf70112284a4eeac151a9
justinfei9 <jpf99@yahoo.com>
2024-02-19 16:35:04 -0500

    checkstyle fix?
    hopefully another fix?

--- src/com/lucas/UserToComputeEngineImp.java
+++ src/com/lucas/UserToComputeEngineImp.java
@@ -2,15 +2,16 @@ package com.lucas;
 
 public class UserToComputeEngineImp implements UserToComputeEngineAPI {
 
-	private final StorageToComputeEngineAPI datastore;
-	private final ComputeEngine computeengine;
+    private final StorageToComputeEngineAPI datastore;
+    private final ComputeEngine computeengine;
 
-	public UserToComputeEngineImp(StorageToComputeEngineAPI datastore, ComputeEngine computeengine) {
-		this.datastore = datastore;
-		this.computeengine = computeengine;
-	}
-	//@Override
-	public ComputeResult compute(ComputeRequest request) {
-		return ComputeResult.FAILURE;
-	}
+    public UserToComputeEngineImp(StorageToComputeEngineAPI datastore, ComputeEngine computeengine) {
+        this.datastore = datastore;
+        this.computeengine = computeengine;
+    }
+
+    //@Override
+    public ComputeResult compute(ComputeRequest request) {
+        return ComputeResult.FAILURE;
+    }
 }
