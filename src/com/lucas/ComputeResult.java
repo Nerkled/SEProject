f82ccf833b48008801c74c31533b242e70815cae
justinfei9 <jpf99@yahoo.com>
2024-02-19 16:29:56 -0500

    checkstyle fix
    checkstyle fix

--- src/com/lucas/ComputeResult.java
+++ src/com/lucas/ComputeResult.java
@@ -1,13 +1,15 @@
 package com.lucas;
 
-public interface ComputeResult {
+public interface ComputeRequest {
+
     static ComputeResult SUCCESS = () -> Status.SUCCESS;
     static ComputeResult FAILURE = () -> Status.FAILURE;
-    
-	Status getStatus();
-	public static enum Status {
-		SUCCESS,
-		FAILURE,
-		NOT_AN_INTEGER;
-	}
+
+    Status getStatus();
+
+    public static enum Status {
+        SUCCESS,
+        FAILURE,
+        NOT_AN_INTEGER;
+    }
 }
