94c5e6c6f72ef8317d46a69fea36b005137eee42
justinfei9 <jpf99@yahoo.com>
2024-02-20 15:41:55 -0500

    checkstyle error

--- test/com/lucas2/InMemoryImpStorageToComputeEngine.java
+++ test/com/lucas2/InMemoryImpStorageToComputeEngine.java
@@ -1,14 +1,16 @@
 package com.lucas2;
 import java.util.ArrayList;
-
+import com.lucas.inputConfig;
+import com.lucas.outputConfig;
 import java.util.List;
 
 public class InMemoryImpStorageToComputeEngine{
     private List<Integer> inputIntegers;
     private List<String> outputStrings;
     public InMemoryImpStorageToComputeEngine(){
-        this.inputIntegers = new ArrayList<>();
-        this.outputStrings = new ArrayList<>();
+        this.inputIntegers = new ArrayList<Integer>(inputConfig.getIntegers());
+        this.outputStrings = new ArrayList<String>(outputConfig.getStrings());
+        
     }
     public void readInput(InMemoryInputConfig inputConfig){
         this.inputIntegers = new ArrayList<>(inputConfig.getIntegers());
