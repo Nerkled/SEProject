5d5ed0963cbd0de47155551868cf01d2fe390575
Henry <henrybecker1014@gmail.com>
2024-02-20 15:14:28 -0500

    Update ComputeTest.java
    Fixed for YML garbage.

--- test/com/lucas2/ComputeTest.java
+++ test/com/lucas2/ComputeTest.java
@@ -1,21 +1,20 @@
 package com.lucas2;
+
 import org.junit.Test;
 import com.lucas.ComputeEngine;
 import com.lucas.ImpComputeEngine;
 
-// Import  Mockito methods 
+// Import Mockito methods
 import org.junit.Assert;
-
-
 
 public class ComputeTest {
 
-	@Test
-	public void smokeTestCompute() {
-		// The computation component has very simple inputs/outputs and no dependencies, so we can
-		// write a smoke test with no mocks at all
-		
-		ComputeEngine engine = new ImpComputeEngine();
-		Assert.assertEquals("1", engine.compute(1));
-	}
+    @Test
+    public void smokeTestCompute() {
+        // The computation component has very simple inputs/outputs and no dependencies, so we can
+        // write a smoke test with no mocks at all
+        
+        ComputeEngine engine = new ImpComputeEngine();
+        Assert.assertEquals("1", engine.compute(1));
+    }
 }
