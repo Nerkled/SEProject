932891ac3135b44844a01e54b68942efab5d8271
vincentbonet <141579416+vincentbonet@users.noreply.github.com>
2024-02-20 15:26:12 -0500

    whitespace fixes

--- test/com/lucas2/TestStorageToComputeEngine.java
+++ test/com/lucas2/TestStorageToComputeEngine.java
@@ -1,4 +1,5 @@
 package com.lucas2;
+
 import com.lucas.StorageToComputeEngineAPI;
 import com.lucas.StorageToComputeEngineImp;
 import com.lucas.Result.ResultStatus;
@@ -7,18 +8,20 @@ import com.lucas.OutputConfig;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+
 public class TestStorageToComputeEngine {
+
     @Test
     public void smokeTestRead() {
         InputConfig inputConfig = Mockito.mock(InputConfig.class);
         StorageToComputeEngineAPI storage = new StorageToComputeEngineImp();
         Assert.assertEquals(1L, storage.read(inputConfig));
-        }
-	
-	@Test
-	public void smokeTestWrite() {
-		OutputConfig outputConfig = Mockito.mock(OutputConfig.class);
-		StorageToComputeEngineAPI storage = new StorageToComputeEngineImp();
-		Assert.assertEquals(ResultStatus.SUCCESS, storage.write(outputConfig, "result").getResult());
-	}
+    }
+
+    @Test
+    public void smokeTestWrite() {
+        OutputConfig outputConfig = Mockito.mock(OutputConfig.class);
+        StorageToComputeEngineAPI storage = new StorageToComputeEngineImp();
+        Assert.assertEquals(ResultStatus.SUCCESS, storage.write(outputConfig, "result").getResult());
+    }
 }
