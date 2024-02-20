b72d514689ccb4a2f428a9c8abcd7bbad2123628
justinfei9 <147112015+justinfei9@users.noreply.github.com>
2024-02-20 15:53:48 -0500

    removed an unused enum (#45)
    removed an unused enum

--- src/com/lucas/UserToComputeEngineAPI.java
+++ src/com/lucas/UserToComputeEngineAPI.java
@@ -1,15 +1,6 @@
+//Justin Feinman
 package com.lucas;
 public interface UserToComputeEngineAPI{
     //API 1
-    ComputeResult compute(ComputeRequest request);
-
-
-    public enum DelimiterResponse {
-        DEFAULT,
-        CUSTOM;
-    }
-
-
-    
-    
+    ComputeResult compute(ComputeRequest request);    
 }
