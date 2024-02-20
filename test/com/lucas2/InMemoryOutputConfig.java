8f939a92a4b6cf69b82b9c7570221dfa32708a07
justinfei9 <jpf99@yahoo.com>
2024-02-20 14:41:09 -0500

    fixed our test classes
    fixed our test classes to work with new API's and implementations.

---
+++ test/com/lucas2/InMemoryOutputConfig.java
@@ -0,0 +1,12 @@
+package com.lucas2;
+import java.util.ArrayList;
+import java.util.List;
+import com.lucas.OutputConfig;
+
+public class InMemoryOutputConfig implements OutputConfig {
+    private final List<String> output = new ArrayList<>();
+
+    public List<String> getStrings() {
+        return new ArrayList<>(output);
+    }
+}
