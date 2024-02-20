2e193c8f4a7b7be18d962ff52e07202a8e082e77
vincentbonet <141579416+vincentbonet@users.noreply.github.com>
2024-02-20 15:24:47 -0500

    whitespace fixes

--- test/com/lucas2/InMemoryImpStorageToComputeEngine.java
+++ test/com/lucas2/InMemoryImpStorageToComputeEngine.java
@@ -4,16 +4,15 @@ import com.lucas.OutputConfig;
 import com.lucas.Result;
 import com.lucas.StorageToComputeEngineAPI;
 public class InMemoryImpStorageToComputeEngine implements StorageToComputeEngineAPI {
-   
+
     @Override
     public Iterable<Integer> read(InputConfig input) {
         // Test code is allowed to assume it's getting the right types; this will fail with a ClassCastException if it gets
         // another type of input. For production code, we'd want some better user input validation
         return ((InMemoryInputConfig)input).getIntegers();
     }
-
     @Override
-    public Result write (OutputConfig output, String result) {
+    public Result write(OutputConfig output, String result) {
         // Test code is allowed to assume it's getting the right types; this will fail with a ClassCastException if it gets
         // another type of input. For production code, we'd want some better user input validation
         ((InMemoryOutputConfig)output).getStrings().add(result);
