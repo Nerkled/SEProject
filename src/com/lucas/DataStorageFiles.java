fdb28cb54f6677ae729734c157d5fccf63cd591e
robertvbonet <141579416+vincentbonet@users.noreply.github.com>
2024-02-21 14:58:02 -0500

    created DataStorageFiles file, implemented read and write methods to create a project that reads in user-specified files (#48)

---
+++ src/com/lucas/DataStorageFiles.java
@@ -0,0 +1,52 @@
+package com.lucas;
+
+import java.io.FileWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class DataStorageFiles implements StorageToComputeEngineAPI {
+
+    private String filePath;
+
+    public DataStorageFiles(String filePath) {
+        this.filePath = filePath;
+    }
+
+    @Override
+    public Iterable<Integer> read(InputConfig input) {
+        List<Integer> list = new ArrayList<>();
+        try (Scanner scanner = new Scanner(new File(filePath))) {
+            while (scanner.hasNextLine()) {
+                String data = scanner.nextLine();
+                try {
+                    int value = Integer.parseInt(data);
+                    list.add(value);
+                } catch (NumberFormatException e) {
+                    System.err.println("Error parsing data: " + data);
+                    e.printStackTrace();
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.err.println("File not found: " + filePath);
+            e.printStackTrace();
+        }
+        return list;
+    }
+
+    @Override
+    public Result write(OutputConfig output, String result) {
+        try (FileWriter fileWriter = new FileWriter(filePath)) {
+            fileWriter.write(result);
+            System.out.println("Write operation successful!");
+            return () -> Result.ResultStatus.SUCCESS;
+        } catch (IOException e) {
+            System.err.println("Error writing to file: " + filePath);
+            e.printStackTrace();
+            return () -> Result.ResultStatus.FAILURE;
+        }
+    }
+}
