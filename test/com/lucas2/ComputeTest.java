71c845800d9e07fa5de72ee5872d3ea9d2e56b0a
justinfei9 <jpf99@yahoo.com>
2024-02-21 16:36:36 -0500

    added usertocomputeengine computation
    added usertocomputeengine computation. I had to fix both other computations to work with my own computation. Made them return and take in a list Integer instead of a int. Part 1,2,3 of assignment 4 should now work

--- test/com/lucas2/ComputeTest.java
+++ test/com/lucas2/ComputeTest.java
@@ -3,7 +3,8 @@ package com.lucas2;
 import org.junit.Test;
 import com.lucas.ComputeEngine;
 import com.lucas.ImpComputeEngine;
-
+import java.util.ArrayList;
+import java.util.List;
 // Import Mockito methods
 import org.junit.Assert;
 
@@ -13,8 +14,9 @@ public class ComputeTest {
     public void smokeTestCompute() {
         // The computation component has very simple inputs/outputs and no dependencies, so we can
         // write a smoke test with no mocks at all
-        
+        List<Integer> values = new ArrayList<>();
+        values.add(1);
         ComputeEngine engine = new ImpComputeEngine();
-        Assert.assertEquals("1", engine.compute(1));
+        Assert.assertEquals("1", engine.compute(values));
     }
 }
