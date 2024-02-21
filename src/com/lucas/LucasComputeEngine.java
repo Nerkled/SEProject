8666c801198a503fd091453f89bac1d472419875
Henry <henrybecker1014@gmail.com>
2024-02-21 14:58:21 -0500

    Fixed yml errors.

--- src/com/lucas/LucasComputeEngine.java
+++ src/com/lucas/LucasComputeEngine.java
@@ -13,8 +13,12 @@ public class LucasComputeEngine implements ComputeEngine {
 
     // Recursive method to compute the nth Lucas number
     private int lucas(int n) {
-        if (n == 0) return 2;
-        if (n == 1) return 1;
+        if (n == 0) {
+            return 2;
+        }
+        if (n == 1) {
+            return 1;
+        }
         return lucas(n - 1) + lucas(n - 2);
     }
 }
