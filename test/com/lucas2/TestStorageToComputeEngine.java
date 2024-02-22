de61732a92bb67ea83d305c87d23b8972e1dd0de
robertvbonet <141579416+vincentbonet@users.noreply.github.com>
2024-02-22 14:13:53 -0500

    tests now pass for smoketestRead and smoketestWrite,  (#52)
    * created DataStorageFiles file, implemented read and write methods to create a project that reads in user-specified files
    
    * fixed my tests, I was getting a return value of null instead of what the test was expecting, which was 1.

--- test/com/lucas2/TestStorageToComputeEngine.java
+++ test/com/lucas2/TestStorageToComputeEngine.java
@@ -1,21 +1,23 @@
 package com.lucas2;
-
 import com.lucas.StorageToComputeEngineAPI;
 import com.lucas.StorageToComputeEngineImp;
 import com.lucas.Result.ResultStatus;
 import com.lucas.InputConfig;
 import com.lucas.OutputConfig;
+import static org.junit.Assert.assertEquals;
+import java.util.List;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
-
 public class TestStorageToComputeEngine {
 
     @Test
     public void smokeTestRead() {
         InputConfig inputConfig = Mockito.mock(InputConfig.class);
         StorageToComputeEngineAPI storage = new StorageToComputeEngineImp();
-        Assert.assertEquals(1L, storage.read(inputConfig));
+        List<Integer> resultList = (List<Integer>) storage.read(inputConfig);
+        Integer result = resultList.get(0);
+        assertEquals(Integer.valueOf(1), result);
     }
 
     @Test
