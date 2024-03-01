b37c00e859d57a151c7b8239080df946194e4264
justinfei9 <jpf99@yahoo.com>
2024-02-29 21:32:24 -0500

    test files
    adding test files

---
+++ test/com/lucas2/TestUser.java
@@ -0,0 +1,26 @@
+package com.lucas2;
+
+import java.io.File;
+
+import com.lucas.UserToComputeEngineAPI;
+
+
+public class TestUser {
+	
+	// TODO 3: change the type of this variable to the name you're using for your
+	// User <-> ComputeEngine API; also update the parameter passed to the constructor
+	private final UserToComputeEngineAPI coordinator;
+
+	public TestUser(UserToComputeEngineAPI coordinator) {
+		this.coordinator = coordinator;
+	}
+
+	public void run(String outputPath) {
+		char delimiter = ';';
+		String inputPath = "test" + File.separatorChar + "testInputFile.test";
+		
+		// TODO 4: Call the appropriate method(s) on the coordinator to get it to 
+		// run the compute job specified by inputPath, outputPath, and delimiter
+	}
+
+}
