ad7e3e6cf43c45b2c2b1a0111225c7d5d488b87c
justinfei9 <jpf99@yahoo.com>
2024-02-29 21:34:58 -0500

    checkstyle fix
    fixing checkstyle?

--- test/com/lucas2/TestUser.java
+++ test/com/lucas2/TestUser.java
@@ -4,23 +4,22 @@ import java.io.File;
 
 import com.lucas.UserToComputeEngineAPI;
 
-
 public class TestUser {
-	
-	// TODO 3: change the type of this variable to the name you're using for your
-	// User <-> ComputeEngine API; also update the parameter passed to the constructor
-	private final UserToComputeEngineAPI coordinator;
 
-	public TestUser(UserToComputeEngineAPI coordinator) {
-		this.coordinator = coordinator;
-	}
+    // TODO 3: change the type of this variable to the name you're using for your
+    // User <-> ComputeEngine API; also update the parameter passed to the constructor
+    private final UserToComputeEngineAPI coordinator;
 
-	public void run(String outputPath) {
-		char delimiter = ';';
-		String inputPath = "test" + File.separatorChar + "testInputFile.test";
-		
-		// TODO 4: Call the appropriate method(s) on the coordinator to get it to 
-		// run the compute job specified by inputPath, outputPath, and delimiter
-	}
+    public TestUser(UserToComputeEngineAPI coordinator) {
+        this.coordinator = coordinator;
+    }
+
+    public void run(String outputPath) {
+        char delimiter = ';';
+        String inputPath = "test" + File.separatorChar + "testInputFile.test";
+
+        // TODO 4: Call the appropriate method(s) on the coordinator to get it to 
+        // run the compute job specified by inputPath, outputPath, and delimiter
+    }
 
 }
