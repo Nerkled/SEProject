71d1f968665eeddfe2f15b70e5de677e05f36e0a
justinfei9 <jpf99@yahoo.com>
2024-03-01 17:04:11 -0500

    checkstyle fix

--- src/com/lucas/OutputConfig.java
+++ src/com/lucas/OutputConfig.java
@@ -2,15 +2,15 @@ package com.lucas;
 
 public interface OutputConfig {
 
-	static interface OutputConfigVisitor {
-		public void visitFile(FileOutputConfig fileOutputConfig);
-	}
-	
-	static void visitOutputConfig(OutputConfig config, OutputConfigVisitor visitor) {
-		if (config instanceof FileOutputConfig) {
-			visitor.visitFile((FileOutputConfig) config);
-		} else {
-			throw new RuntimeException("Unexpected config type: " + config.getClass());
-		}
-	}
+    static interface OutputConfigVisitor {
+        public void visitFile(FileOutputConfig fileOutputConfig);
+    }
+
+    static void visitOutputConfig(OutputConfig config, OutputConfigVisitor visitor) {
+        if (config instanceof FileOutputConfig) {
+            visitor.visitFile((FileOutputConfig) config);
+        } else {
+            throw new RuntimeException("Unexpected config type: " + config.getClass());
+        }
+    }
 }
