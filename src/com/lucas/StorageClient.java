7710eaac3a6ad996902a11658ac2f6441ecbebe1
vincentbonet <141579416+vincentbonet@users.noreply.github.com>
2024-04-09 16:24:36 -0400

    Refactor build.gradle and gradle/build.gradle files, and delete UserToComputeProto.java

---
+++ src/com/lucas/StorageClient.java
@@ -0,0 +1,47 @@
+package com.lucas;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import main.proto.*;
+import main.proto.UserToComputeProto.*;
+
+public class StorageClient {
+    private final ManagedChannel channel;
+    private final DataStoreGrpc.DataStoreBlockingStub blockingStub;
+
+    public StorageClient(String host, int port) {
+        this(ManagedChannelBuilder.forAddress(host, port).usePlaintext().build());
+    }
+
+    public StorageClient(ManagedChannel channel) {
+        this.channel = channel;
+        this.blockingStub = DataStoreGrpc.newBlockingStub(channel);
+    }
+
+    public void shutdown() throws InterruptedException {
+        channel.shutdown().awaitTermination(5, java.util.concurrent.TimeUnit.SECONDS);
+    }
+
+    public int getData(int n) {
+        GetDataRequest request = GetDataRequest.newBuilder().setN(n).build();
+        GetDataResponse response = blockingStub.getData(request);
+        return response.getResult();
+    }
+
+    public boolean storeData(String key, String value) {
+        StoreDataRequest request = StoreDataRequest.newBuilder().setKey(key).setValue(value).build();
+        StoreDataResponse response = blockingStub.storeData(request);
+        return response.getSuccess();
+    }
+
+    public static void main(String[] args) throws Exception {
+        StorageClient client = new StorageClient("localhost", 50051);
+
+        int result = client.getData(123);
+        System.out.println("GetData response: " + result);
+
+        boolean success = client.storeData("key1", "value1");
+        System.out.println("StoreData response: " + success);
+
+        client.shutdown();
+    }
+}
