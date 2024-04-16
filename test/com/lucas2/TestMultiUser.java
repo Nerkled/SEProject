aea885b519e1e1da5a655d819b2b20a87ddc7549
justinfei9 <jpf99@yahoo.com>
2024-04-16 16:40:48 -0400

    fixed testMultiUser
    fixed testMultiUser to work, this in turn fixed DataStorageFiles, TestUser as Well

--- test/com/lucas2/TestMultiUser.java
+++ test/com/lucas2/TestMultiUser.java
@@ -10,9 +10,11 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
 import com.lucas.ComputeEngine;
+import com.lucas.DataStorageFiles;
 import com.lucas.LucasComputeEngine;
 import com.lucas.StorageToComputeEngineAPI;
 import com.lucas.StorageToComputeEngineImp;
+import com.lucas.UserToComputeEngineAPI;
 import com.lucas.UserToComputeEngineImp;
 
 import org.junit.Assert;
@@ -24,7 +26,7 @@ public class TestMultiUser {
 
     // TODO 1: change the type of this variable to the name you're using for your
     // User <-> ComputeEngine API
-    private UserToComputeEngineImp coordinator;
+    private UserToComputeEngineAPI coordinator;
     private StorageToComputeEngineAPI dataStorage;
     private ComputeEngine engine;
 
@@ -34,7 +36,7 @@ public class TestMultiUser {
         // component
         // that the user will make requests to
         // Store it in the 'coordinator' instance variable
-        dataStorage = new StorageToComputeEngineImp();
+        dataStorage = new DataStorageFiles();
         engine = new LucasComputeEngine(); //make it imp of this
         coordinator = new UserToComputeEngineImp(dataStorage, engine);
 
@@ -77,12 +79,12 @@ public class TestMultiUser {
         });
 
         // Check that the output is the same for multi-threaded and single-threaded
-        /* Currently this is commented out so i can pass the test to upload this to github. I'm working on a fix right now
+       
 
 
         List<String> singleThreaded = loadAllOutput(singleThreadFilePrefix, numThreads);
         List<String> multiThreaded = loadAllOutput(multiThreadFilePrefix, numThreads);
-        Assert.assertEquals(singleThreaded, multiThreaded); //this should check output for single and multi threaded;*/
+        Assert.assertEquals(singleThreaded, multiThreaded); //this should check output for single and multi threaded;
     }
 
     private List<String> loadAllOutput(String prefix, int numThreads) throws IOException {
--- test/com/lucas2/TestUser.java
+++ test/com/lucas2/TestUser.java
@@ -3,6 +3,7 @@ package com.lucas2;
 import com.lucas.FileInputConfig;
 import com.lucas.FileOutputConfig;
 import com.lucas.Request;
+import com.lucas.UserToComputeEngineAPI;
 import com.lucas.UserToComputeEngineImp;
 
 import java.io.File;
@@ -14,17 +15,17 @@ public class TestUser {
     
     // TODO 3: change the type of this variable to the name you're using for your
     // User <-> ComputeEngine API; also update the parameter passed to the constructor
-    private final UserToComputeEngineImp coordinator;
+    private final UserToComputeEngineAPI coordinator;
 
-    public TestUser(UserToComputeEngineImp coordinator) {
-        this.coordinator = coordinator;
+    public TestUser(UserToComputeEngineAPI coordinator2) {
+        this.coordinator = coordinator2;
     }
 
     // TODO 4: Call the appropriate method(s) on the coordinator to get it to 
     // run the compute job specified by inputPath, outputPath, and delimiter
     public void run(String outputPath) {
         char delimiter = ';';
-        String inputPath = "test" + File.separatorChar + "testInputFile.test";
+        String inputPath = "test" + File.separatorChar + "com" + File.separatorChar + "lucas2" + File.separatorChar + "testInputFile.test";
 
         FileInputConfig input = new FileInputConfig(inputPath);
         FileOutputConfig output = new FileOutputConfig(outputPath);
