5bd41ec22b386471a1b53b14c7547ee97efddd23
vincentbonet <141579416+vincentbonet@users.noreply.github.com>
2024-04-30 17:22:08 -0400

    updated our main
    main updated to use correct apis

--- src/com/lucas/Main.java
+++ src/com/lucas/Main.java
@@ -1,14 +1,24 @@
 package com.lucas;
 import java.io.File;
 
+/*
+ *  Make a @test for benchmark test, include the old way
+ * and the new way and maybe compare speed or cpu usage
+ * document this in repo
+ * 
+ *
+ * 
+ */
 public class Main {
     public static void main(String[] args) {
         StorageToComputeEngineAPI dataStorage = new DataStorageFiles();
         ComputeEngine engine = new LucasComputeEngine("localhost", 0);
-        UserToComputeEngineAPI coordinator = new UserToComputeEngineComponent(dataStorage, engine,7);
+        DataStorageFiles dataStorageFiles = new DataStorageFiles();
+        LucasComputeEngine computeComponent = new LucasComputeEngine("localhost", 0);
+        UserToComputeEngineAPI coordinator = new UserToComputeEngineComponent(dataStorageFiles, computeComponent,10);
         char delimiter = ';';
         String inputPath = "test" + File.separatorChar + "com" + File.separatorChar + "lucas2" + File.separatorChar + "testInputFileLarge.test";
-        String outputPath = "your_output_path_here";
+        String outputPath = "LucasOutputLarge";
 
         FileInputConfig input = new FileInputConfig(inputPath);
         FileOutputConfig output = new FileOutputConfig(outputPath);
