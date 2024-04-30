07667aa56cdf5ca8a11893a16732fb3a450db1e5
robertvbonet <141579416+vincentbonet@users.noreply.github.com>
2024-04-30 17:26:13 -0400

    made compute engine run better, made a benchmark  test, updated the api (#104)
    * Create .gitignore
    
    * added a main and a larger testInputFile
    
    * Create your_output_path_here
    
    * Revert "Create your_output_path_here"
    
    This reverts commit dc6df2564fb42615c728665ded401e48f290b207.
    
    * made compute engine run better and made  a test for it
    
    made compute engine run better, made a benchmark  test, updated the api
    
    * updated our main
    
    main updated to use correct apis
    
    ---------
    
    Co-authored-by: justinfei9 <147112015+justinfei9@users.noreply.github.com>
    Co-authored-by: justinfei9 <jpf99@yahoo.com>

---
+++ src/com/lucas/Main.java
@@ -0,0 +1,31 @@
+package com.lucas;
+import java.io.File;
+
+/*
+ *  Make a @test for benchmark test, include the old way
+ * and the new way and maybe compare speed or cpu usage
+ * document this in repo
+ * 
+ *
+ * 
+ */
+public class Main {
+    public static void main(String[] args) {
+        StorageToComputeEngineAPI dataStorage = new DataStorageFiles();
+        ComputeEngine engine = new LucasComputeEngine("localhost", 0);
+        DataStorageFiles dataStorageFiles = new DataStorageFiles();
+        LucasComputeEngine computeComponent = new LucasComputeEngine("localhost", 0);
+        UserToComputeEngineAPI coordinator = new UserToComputeEngineComponent(dataStorageFiles, computeComponent,10);
+        char delimiter = ';';
+        String inputPath = "test" + File.separatorChar + "com" + File.separatorChar + "lucas2" + File.separatorChar + "testInputFileLarge.test";
+        String outputPath = "LucasOutputLarge";
+
+        FileInputConfig input = new FileInputConfig(inputPath);
+        FileOutputConfig output = new FileOutputConfig(outputPath);
+        Request request = new Request(input, output, delimiter);
+        coordinator.compute(request);
+        //write the output of compute inv a print statement
+        ComputeResult result = coordinator.compute(request);
+        System.out.println(result.getStatus());
+    }
+}
