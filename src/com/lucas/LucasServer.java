88f4f0659e0ec1ba6aefc91a58eb92a2c744c21e
justinfei9 <jpf99@yahoo.com>
2024-05-08 15:46:56 -0400

    checkstyle errors

--- src/com/lucas/LucasServer.java
+++ src/com/lucas/LucasServer.java
@@ -1,8 +1,6 @@
 package com.lucas;
-
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
-
 import io.grpc.Grpc;
 import io.grpc.InsecureServerCredentials;
 import io.grpc.Server;
@@ -10,11 +8,9 @@ import io.grpc.protobuf.services.ProtoReflectionService;
 
 public class LucasServer { // Boilerplate TODO: Change name of class
 	private io.grpc.Server server;
-
-	  private void start() throws IOException {
+	private void start() throws IOException{
 	    /* The port on which the server should run */
-	    int port = 50051; 
-	    
+	    int port = 50051;
 	    server = Grpc.newServerBuilderForPort(port, InsecureServerCredentials.create())
 	        .addService(new LucasServerImpl()) // Boilerplate TODO: Change name of class
 	        .addService(ProtoReflectionService.newInstance())
@@ -23,28 +19,28 @@ public class LucasServer { // Boilerplate TODO: Change name of class
 	    System.out.println("Server started on port " + port);
 	    
 	    Runtime.getRuntime().addShutdownHook(new Thread() {
-	      @Override
-	      public void run() {
-	        System.err.println("*** shutting down gRPC server since JVM is shutting down");
-	        try {
-	        	if (server != null) {
-	      	      server.shutdown().awaitTermination(30, TimeUnit.SECONDS);
-	      	    }
-	        } catch (InterruptedException e) {
-	          e.printStackTrace(System.err);
-	        }
-	        System.err.println("*** server shut down");
-	      }
+			@Override
+	      	public void run() {
+	        	System.err.println("*** shutting down gRPC server since JVM is shutting down");
+	        	try {
+	        		if (server != null) {
+	      	      		server.shutdown().awaitTermination(30, TimeUnit.SECONDS);
+	      	    	}
+	        	} catch (InterruptedException e) {
+				e.printStackTrace(System.err);
+	        	}
+	        	System.err.println("*** server shut down");
+	      	}
 	    });
-	  }
+	}
 
 	  /**
 	   * Await termination on the main thread since the grpc library uses daemon threads.
 	   */
 	  private void blockUntilShutdown() throws InterruptedException {
-	    if (server != null) {
-	      server.awaitTermination();
-	    }
+		  if (server != null) {
+			  server.awaitTermination();
+		  }
 	  }
 
 	  public static void main(String[] args) throws Exception {
